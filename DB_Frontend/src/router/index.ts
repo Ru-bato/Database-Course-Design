d66d1bfe0642d6e69e2f9e822b57cb892ef7dc4b
Ru-bato <108850678@qq.com>
2024-08-17 16:29:22 +0800

    update frontend
    update frontend

---
+++ DB_Frontend/src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import Booking from '../views/Booking.vue';
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/AboutView.vue')
+//     }
+//   ]
+// })
+
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/booking', name: 'Booking', component: Booking },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router
---
+++ DB_Frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Welcome to 12306 Railway Ticket System</h1>
+      <button @click="navigateToBooking">Book Tickets</button>
+      <RouterLink to="/login" >Login</RouterLink>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  export default defineComponent({
+    name: 'Home',
+    setup() {
+      const router = useRouter();
+  
+      const navigateToBooking = () => {
+        router.push('/booking');
+      };
+  
+      return {
+        navigateToBooking,
+      };
+    },
+  });
+  </script>
+  
