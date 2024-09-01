0d87fa33d453bce00324a3661ca625588f7d4516
Euuugeo <eugeo1212@gmail.com>
2024-09-01 23:35:44 +0800

    后端-9-1

--- Order_Backend/Order_Backend/Controllers/MyOrderController.cs
+++ Order_Backend/Order_Backend/Controllers/MyOrderController.cs
@@ -23,7 +23,7 @@ namespace Order_Backend.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpGet("GetMyOrder")]
+        [HttpGet("GetMyAllOrder")]
         public IActionResult Get(string? cust)
         {
             using (OracleConnection connection = new OracleConnection(conStr))
@@ -62,5 +62,126 @@ namespace Order_Backend.Controllers
             }
                 
         }
+        [HttpGet("GetMyPaidOrder")]
+        public IActionResult GetPaid(string? cust)
+        {
+            using (OracleConnection connection = new OracleConnection(conStr))
+            {
+                try
+                {
+                    //var ruleCodes = string.IsNullOrEmpty(cust) ? _dbContext.ORDERLIST
+                    //    .ToList() : _dbContext.ORDERLIST.Where(code => code.User_id == cust)
+                    //    .ToList();
+                    var ruleCodes = from ticket in _dbContext.ORDERLIST
+                                    join train in _dbContext.TRAIN
+                                    on ticket.Train_id equals train.Train_id
+                                    where ticket.User_id == cust
+                                    where ticket.Order_status=="paid"
+                                    select new
+                                    {
+                                        OrderId = ticket.Order_id,
+                                        UserId = ticket.User_id,
+                                        TrainId = train.Train_id,
+                                        PassengerId = ticket.Passenger_id,
+                                        OrderStatus = ticket.Order_status,
+                                        Price = ticket.Price,
+                                        TicketType = ticket.Ticket_type,
+                                        DepartureStation = train.Departure_station,
+                                        ArrivalStation = train.Arrival_station,
+                                        DepartureTime = train.Departure_time,
+                                        ArriveTime = train.Arrival_time
+                                    };
+
+                    return Ok(ruleCodes);
+
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.Message);
+                }
+            }
+
+        }
+        [HttpGet("GetMyUnpaidOrder")]
+        public IActionResult GetUnpaid(string? cust)
+        {
+            using (OracleConnection connection = new OracleConnection(conStr))
+            {
+                try
+                {
+                    //var ruleCodes = string.IsNullOrEmpty(cust) ? _dbContext.ORDERLIST
+                    //    .ToList() : _dbContext.ORDERLIST.Where(code => code.User_id == cust)
+                    //    .ToList();
+                    var ruleCodes = from ticket in _dbContext.ORDERLIST
+                                    join train in _dbContext.TRAIN
+                                    on ticket.Train_id equals train.Train_id
+                                    where ticket.User_id == cust
+                                    where ticket.Order_status=="unpaid"
+                                    select new
+                                    {
+                                        OrderId = ticket.Order_id,
+                                        UserId = ticket.User_id,
+                                        TrainId = train.Train_id,
+                                        PassengerId = ticket.Passenger_id,
+                                        OrderStatus = ticket.Order_status,
+                                        Price = ticket.Price,
+                                        TicketType = ticket.Ticket_type,
+                                        DepartureStation = train.Departure_station,
+                                        ArrivalStation = train.Arrival_station,
+                                        DepartureTime = train.Departure_time,
+                                        ArriveTime = train.Arrival_time
+                                    };
+
+                    return Ok(ruleCodes);
+
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.Message);
+                }
+            }
+
+        }
+        [HttpGet("GetMyWaitOrder")]
+        public IActionResult GetWait(string? cust)
+        {
+            using (OracleConnection connection = new OracleConnection(conStr))
+            {
+                try
+                {
+                    //var ruleCodes = string.IsNullOrEmpty(cust) ? _dbContext.ORDERLIST
+                    //    .ToList() : _dbContext.ORDERLIST.Where(code => code.User_id == cust)
+                    //    .ToList();
+                    var ruleCodes = from ticket in _dbContext.ORDERLIST
+                                    join train in _dbContext.TRAIN
+                                    on ticket.Train_id equals train.Train_id
+                                    where ticket.User_id == cust
+                                    where ticket.Order_status =="wait"
+                                    select new
+                                    {
+                                        OrderId = ticket.Order_id,
+                                        UserId = ticket.User_id,
+                                        TrainId = train.Train_id,
+                                        PassengerId = ticket.Passenger_id,
+                                        OrderStatus = ticket.Order_status,
+                                        Price = ticket.Price,
+                                        TicketType = ticket.Ticket_type,
+                                        DepartureStation = train.Departure_station,
+                                        ArrivalStation = train.Arrival_station,
+                                        DepartureTime = train.Departure_time,
+                                        ArriveTime = train.Arrival_time
+                                    };
+
+                    return Ok(ruleCodes);
+
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.Message);
+                }
+            }
+
+        }
+
     }
 }
