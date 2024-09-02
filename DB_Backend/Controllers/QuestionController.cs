d8c333a66b73c3fc7709f5407162761b52355065
保留实力 <127399267+bshtj@users.noreply.github.com>
2024-09-02 16:55:10 +0800

    指南后端

---
+++ DB_Backend/Controllers/QuestionController.cs
@@ -0,0 +1,42 @@
+﻿using DB_Backend.DB_BackendBLL;
+using DB_Backend.DB_BackendModel;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace DB_Backend.Controllers
+{
+    public class QuestionRequest
+    {
+        public string QuestionSearch { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetQuestion()
+        {
+            List<QuestionModel> model = QuestionManager.ReturnList();
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public IActionResult PostQuestion([FromBody] QuestionRequest request)
+        {
+            if (!string.IsNullOrEmpty(request.QuestionSearch))
+            {
+                var result = QuestionManager.SearchRelatedQuestions(request.QuestionSearch);
+                var response = new
+                {
+                    Questions = result.Item1,
+                    Answers = result.Item2
+                };
+                return Ok(response);
+            }
+
+            return BadRequest("QuestionSearch parameter is missing or empty");
+        }
+    }
+}
---
+++ DB_Backend/DB_BackendDAL/QuestionServer.cs
@@ -0,0 +1,32 @@
+﻿using DB_Backend.DB_BackendModel;
+using DB_Backend.DB_backendDAL;
+using System.Data;
+
+namespace DB_Backend.DB_BackendDAL
+{
+    public class QuestionServer
+    {
+        public static DataTable ReturnList()
+        {
+            string sql = @"SELECT *
+                       FROM QUESTION";
+            DataTable dt = DBServer.FetchData(sql);
+            return dt;
+           
+        }
+
+        public static DataTable SearchInSQL(string questionSearch)
+        {
+            string sql = @"SELECT QUESTION_TEXT, QUESTION_ANSWER FROM QUESTION WHERE QUESTION_TEXT LIKE '%' || :questionSearch || '%'";
+            // 创建参数字典
+            var parameters = new Dictionary<string, object>
+    {
+        { "@questionSearch", questionSearch }
+    };
+
+            // 使用 DBServer.FetchData 方法执行查询并返回 DataTable
+            DataTable dt = DBServer.FetchData(sql, parameters);
+            return dt;
+        }
+    }
+}
