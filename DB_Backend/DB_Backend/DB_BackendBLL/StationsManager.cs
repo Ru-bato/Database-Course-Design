4e569e653a0a85dfad3a26f03c9f5903eb66b025
WWWJ66 <846198725@qq.com>
2024-09-02 16:03:21 +0800

    基本完成index主页的前端和后端

---
+++ DB_Backend/DB_Backend/DB_BackendBLL/StationsManager.cs
@@ -0,0 +1,50 @@
+﻿using DB_Backend.DB_backendDAL;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace DB_Backend.DB_BackendBLL
+{
+    public class StationsManager
+    {   
+        // 获取所有的车站名 
+        public static List<string> GetStationsNames()
+        {
+            var names = new List<string>();
+
+            DataTable dt = StationsServer.GetStationsNames();
+            names = ExtractStationName(dt);
+
+            return names;
+        }
+
+        // 搜索车站名(根据关键词)
+        public static List<string> SearchStationsByKW(string query) {
+            var names = new List<string>();
+
+            DataTable dt = StationsServer.SearchStationsByKW(query);
+            names =ExtractStationName(dt);
+
+            return names;
+        }
+
+        // 获取表中STATION_NAME
+        private static List<string> ExtractStationName(DataTable dt)
+        {
+            var names = new List<string>();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr["STATION_NAME"] != DBNull.Value)
+                {
+                    string? name = dr["STATION_NAME"].ToString();
+                    if (name != null)
+                    {
+                        names.Add(name);
+                    }
+                }
+            }
+
+            return names;
+        }
+    }
+}
---
+++ db_frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <IndexView/>
+</template>
+
+<script>
+import IndexView from './views/IndexView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IndexView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
