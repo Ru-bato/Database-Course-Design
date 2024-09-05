a2f4a7ab244ba62dff2c519b56e1f54233598378
unicorn888888 <152460738+unicorn888888@users.noreply.github.com>
2024-09-06 03:13:04 +0800

    更新后端

---
+++ DB_Backend/DB_Backend/Controllers/FoodServerController.cs
@@ -0,0 +1,19 @@
+﻿using DB_Backend.DB_BackendBLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DB_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+        // GET api/food
+        public class FoodServerController : Controller
+        {
+            // GET api/stations
+            [HttpGet]
+            public IActionResult GetFoodNames()
+            {
+                var names = FoodManager.GetFoodNames();
+                return Ok(names);
+            }
+        }
+}
