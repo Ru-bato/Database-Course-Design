e674a8f2246eeab98b9736519820ed06cc5bd0fb
WWWJ66 <846198725@qq.com>
2024-09-06 13:16:32 +0800

    前端新增跳转功能

--- DB_Frontend/src/router/index.ts
+++ DB_Frontend/src/router/index.ts
@@ -12,6 +12,7 @@ import WaitOrder from '@/views/WaitOrder.vue'
 import TicketsQuestions from '@/views/TicketsQuestions.vue'
 import StationPage from '@/views/StationPage.vue'
 import Index from '@/views/IndexPage.vue'
+<<<<<<< Updated upstream
 
 // const router = createRouter({
 //   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,12 @@ import Index from '@/views/IndexPage.vue'
 //     }
 //   ]
 // })
+=======
+import VerifyIdentityPage from '@/views/VerifyIdentityPage.vue'
+import ResetPasswordPage from '@/views/ResetPasswordPage.vue'
+import PersonalPage from '@/views/PersonalPage.vue'
+// import TicketShow from ''
+>>>>>>> Stashed changes
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'Home', component: Home },
@@ -44,7 +51,13 @@ const routes: Array<RouteRecordRaw> = [
   { path: '/waitOrder', name: 'WaitOrder', component: WaitOrder },
   { path: '/ticketsQuestions', name: 'TicketsQuestions', component: TicketsQuestions },
   { path: '/stationPage', name: 'StationPage', component: StationPage },
+<<<<<<< Updated upstream
   { path: '/index', name: 'Index', component: Index }
+=======
+  { path: '/index', name: 'Index', component: Index },
+  { path: '/personal', name: 'PersonalPage', component: PersonalPage, meta: { requiresAuth: true } },
+  // { path: '/ticketShow',name: 'TicketShowPage', component: TicketShow}
+>>>>>>> Stashed changes
 ]
 
 const router = createRouter({
